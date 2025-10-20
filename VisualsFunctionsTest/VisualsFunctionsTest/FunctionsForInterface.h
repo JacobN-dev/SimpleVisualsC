94f3893ec4b0664cf6807e76b4035044d5b5da7a
JacobN-dev <kuba123narloch@wp.pl>
2025-10-20 20:30:42 +0200

    Delete VisualsFunctionsTest/VisualsFunctionsTest/FunctionsForInterface.h

--- VisualsFunctionsTest/VisualsFunctionsTest/FunctionsForInterface.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-	void CleaningConsole() noexcept;
-	void CleaningBuffor() noexcept;
-	void CleanAll() noexcept;
-	void ErrorHandlerCin() noexcept; //wip
-	void AftCleanF() noexcept;
-	void AftCleanP() noexcept;
-	void InBetw() noexcept;
-	void TabSort() noexcept;
