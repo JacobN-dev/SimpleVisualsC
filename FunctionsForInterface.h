8ee9178d67a8f9be05d3221d7602c00fd668bd57
JacobN-dev <kuba123narloch@wp.pl>
2025-10-21 18:52:44 +0200

    Add files via upload

--- FunctionsForInterface.h
+++ FunctionsForInterface.h
@@ -4,14 +4,14 @@
 #include <string>
 
 
-    // functions to use.
-	void CleaningConsole() noexcept , // Use to clean the console, (this is not cleaning the buffer!).
-		CleaningBuffor() noexcept ,  // Use to clean the buffer.
-		CleanAll() noexcept ,  //Use to clean both buffer and console.
-		AftCleanF(std::string title) noexcept , //Use to keep the console clean and to delete the buffer, for example after taking input u can use this before going into another function. //wip
-		AftCleanP(std::string title) noexcept , // Same as AftCleanF but without cleaning the buffer. //wip
-		InBetw() noexcept , // Use to add a visual devisor of content.
-		TabSort() noexcept; // wip
+    // functions to use. >
+	void CleaningConsole() noexcept , 
+		CleaningBuffor() noexcept , 
+		CleanAll() noexcept , 
+		AftCleanF(std::string title) noexcept , 
+		AftCleanP(std::string title) noexcept , 
+		InBetw() noexcept , 
+		TabSort() noexcept; 
 
 
 
@@ -23,11 +23,7 @@
 
 
 	std::string title = "Empty title";
-	double SpacesPerSide = 121 - title.length();
-	double SpaceCalc() 
-	{
-		//wip (Maybe change the whole idea, just devide and put around the title, problem appears when your title is even, gotta -0.5 and then generate.)
-	}
+	
 	void CleaningBuffor() noexcept { std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); }
 	void CleaningConsole() noexcept
 	{
@@ -40,25 +36,49 @@
 	void CleanAll() noexcept { CleaningBuffor(); CleaningConsole();}
 	void AftCleanF(std::string title) noexcept 
 	{ 
+		if (title.length() > 121) { title = "(((Title too long!!!)))"; }
+		int Spaces = (121 - title.length()) ,
+		Filler = Spaces % 2,
+		SpacesPerSide = (Spaces/2) + Filler;
 	CleaningConsole(); 
 	std::cout
-		<< "+----------------------------------------------------------------------------------------------------------------------+"
-		<< "                                  "
-		<< SpacesPerSide
-		<< title
-		<< SpacesPerSide
-		<< std::endl;
+			<< "+----------------------------------------------------------------------------------------------------------------------+"
+			<< std::endl;
+		for (int i = 0; i <= SpacesPerSide; i++)
+		{
+			std::cout << " ";
+		}
+		std::cout
+			<< title;
+		for (int i = 0; i <= SpacesPerSide; i++)
+		{
+			std::cout << " ";
+		}
+		std::cout
+			<< std::endl;
 		CleaningBuffor();
 	}
 	void AftCleanP(std::string title) noexcept
 	{
+		if (title.length() > 121) { title = "(((Title too long!!!)))"; }
+		int Spaces = (121 - title.length()) ,
+			Filler = Spaces % 2 ,
+			SpacesPerSide = (Spaces/2);
 		CleaningConsole();
-		
 		std::cout
 			<< "+----------------------------------------------------------------------------------------------------------------------+"
-			<< SpacesPerSide
-			<< title
-			<< SpacesPerSide
+			<< std::endl;
+		for (int i = 1; i <= SpacesPerSide; i++)
+		{
+			std::cout << " ";
+		}
+		std::cout
+			<< title;
+		for (int i = 0; i <= SpacesPerSide; i++)
+		{
+			std::cout << " ";
+		}
+		std::cout
 			<< std::endl;
 	}
 	void InBetw() noexcept
